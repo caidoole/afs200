c78e2daeb1e6f343dbc2c652ceb94763a6d97fe2
caidoole <caitlin.dooley@bryanuniversity.edu>
2021-05-25 10:35:57 -0700

    wk3 take one list and things and such

---
+++ wk3/assignmentTJS/list/stepOne.py
@@ -0,0 +1,8 @@
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#print out all elements less than 5
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 3, 0, 13, 5]
+
+for elem in a:
+    if elem < 5:
+        print(elem)
---
+++ wk3/assignmentTJS/tuple/stepTwo.py
@@ -0,0 +1,9 @@
+#Tuple 1 = Robert = name, surname, birth year, fav movie and year, profession, birthplace
+#Tuple 2 = (1,2,3,4,5,6,7); call value [0] and value 2 between 1-4
+#Goal, run tuple and get vlaue for robert and return 2,3 and 4
+
+tup1 = ('Robert', 'Carlos', '1965', 'Terminator 1995', 'Actor', 'Florida')
+tup2 = (1,2,3,4,5,6,7); 
+
+print(tup1[0])
+print(tup2[1:4])
